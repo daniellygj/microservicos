314f2019448490549e18ed8a370007a8d5432a49
daniellygj <daniellygj@gmail.com>
2021-10-27 13:58:15 -0300

    product model

---
+++ product/src/main/java/com/springcloud/product/model/Product.java
@@ -0,0 +1,44 @@
+package com.springcloud.product.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    
+    private BigDecimal value;
+
+
+    private Product(@NonNull String description, @NonNull BigDecimal value) {
+        setDescription(description);
+        setValue(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product produto = (Product) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}

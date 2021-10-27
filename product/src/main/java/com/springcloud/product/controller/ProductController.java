14b7e0ef6acad57466f668cb40a2d985c0974866
daniellygj <daniellygj@gmail.com>
2021-10-27 14:29:51 -0300

    metodos controller

--- product/src/main/java/com/springcloud/product/controller/ProductController.java
+++ product/src/main/java/com/springcloud/product/controller/ProductController.java
@@ -3,14 +3,13 @@ package com.springcloud.product.controller;
 import com.springcloud.product.model.Product;
 import com.springcloud.product.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
@@ -21,10 +20,18 @@ public class ProductController {
         return repository.findAll();
     }
 
-    @GetMapping("{id}")
+    @GetMapping("/{id}")
     public Optional<Product> findById(@PathVariable Long id) {
         return repository.findById(id);
     }
 
+    @PostMapping
+    public Product insert(@RequestBody Product product) {
+        return repository.save(product);
+    }
 
+    @DeleteMapping
+    public void delete(@PathVariable("id") Long id) {
+        repository.deleteById(id);
+    }
 }
